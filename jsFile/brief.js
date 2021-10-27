2de8481750e9ecd132b0571adae3a19ff597b6b5
Sumant Singh <sumantsingh236@gmail.com>
2021-10-27 14:39:53 +0530

    Changing branch

---
+++ jsFile/brief.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", ()=>{
+    handleData()
+    .then(res =>console.log(res))
+    .catch(err => console.log(err));
+})
+
+function handleData(){
+    return fetch("https://newsapi.org/v2/top-headlines&apiKey=e88a3ca368aa40d6bfd6144b856d172e")
+    .then(res=>res.json())
+    
+}
