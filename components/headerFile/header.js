2e3b79e7a83f5b08c56cbf2d36a672660047e14a
Mausam Kumar <mausam15171098@gmail.com>
2021-10-29 15:07:00 +0530

    completed section five

--- components/headerFile/header.js
+++ components/headerFile/header.js
@@ -26,7 +26,7 @@
     data.forEach(function(ele){
         var newDiv = document.createElement('div')
         newDiv.classList.add('sectionOneHeadlineLeftCard')
-        newP = document.createElement('p')
+        let newP = document.createElement('p')
         newP.innerText = ele.title
         newDiv.append(newP)
         var newA = document.createElement('img')
@@ -131,6 +131,145 @@
         newP.innerText = ele.title
         newDiv.append(newP)
         container.append(newDiv)
+    })
+    mainContainer.append(container)
+})();
+
+// Section Five
+
+(async function sectionOneHeadlineRight(){
+    let data = await fetch('http://localhost:3000/citySectionOne')
+    data = await data.json()
+    // console.log(data);
+    var container = document.createElement('div')
+    container.classList.add('sectionOneHeadlineRightContainer')
+    var mainContainer = document.querySelector('.FiveCityLowerUpperRight')
+    data.forEach(function(ele){
+        var newA = document.createElement('p')
+        newA.classList.add("sectionOneHeadlineRightCard")
+        newA.innerText = "ߋ"+"   "+ele.title.slice(0,52)+"..."
+        container.append(newA)
+    })
+    mainContainer.append(container)
+})();
+
+(async function FiveCityLowerUpperRight(){
+    let data = await fetch('http://localhost:3000/citySectionTwo')
+    data = await data.json()
+    // console.log(data);
+    var container = document.createElement('div')
+    container.classList.add('FiveCityLowerLowerContainer')
+    var mainContainer = document.querySelector('.FiveCityLowerLower')
+    data.forEach(function(ele){
+        var newA = document.createElement('p')
+        // newA.classList.add("sectionOneHeadlineRightCard")
+        newA.innerText = ele.title
+        container.append(newA)
+    })
+    mainContainer.append(container)
+})();
+
+
+(async function sectionOneHeadlineRight(){
+    let data = await fetch('http://localhost:3000/coranaVirusSectionOne')
+    data = await data.json()
+    // console.log(data);
+    var container = document.createElement('div')
+    container.classList.add('sectionOneHeadlineRightContainer')
+    var mainContainer = document.querySelector('.FiveCityLowerUpperRightSecond')
+    data.forEach(function(ele){
+        var newA = document.createElement('p')
+        newA.classList.add("sectionOneHeadlineRightCard")
+        newA.innerText = "ߋ"+"   "+ele.title.slice(0,52)+"..."
+        container.append(newA)
+    })
+    mainContainer.append(container)
+})();
+
+(async function FiveCityLowerUpperRight(){
+    let data = await fetch('http://localhost:3000/coranaVirusSectionTwo')
+    data = await data.json()
+    // console.log(data);
+    var container = document.createElement('div')
+    container.classList.add('FiveCityLowerLowerContainer')
+    var mainContainer = document.querySelector('.FiveCityLowerLowerSecond')
+    data.forEach(function(ele){
+        var newA = document.createElement('p')
+        // newA.classList.add("sectionOneHeadlineRightCard")
+        newA.innerText = ele.title
+        container.append(newA)
+    })
+    mainContainer.append(container)
+})();
+
+(async function sectionOneHeadlineMiddle(){
+    let data = await fetch('http://localhost:3000/indiaFightCovid')
+    data = await data.json()
+    var container = document.createElement('div')
+    // container.classList.add('sectionOneHeadlineMiddleContainer')
+    var mainContainer = document.querySelector('.indiaFightCovid')
+
+    data.forEach(function(ele){
+        var newDiv = document.createElement('div')
+        // newDiv.classList.add('sectionOneHeadlineMiddleCard')
+        var newA = document.createElement('img')
+        newA.src = ele.imageURL
+        newDiv.append(newA)
+        newP = document.createElement('p')
+        newP.innerText = ele.title
+        newDiv.append(newP)
+        container.append(newDiv)
+    })
+
+    mainContainer.append(container)
+})();
+
+(async function sectionOneHeadlineMiddle(){
+    let data = await fetch('http://localhost:3000/sectionFiveExplore')
+    data = await data.json()
+    var container = document.createElement('div')
+    // container.classList.add('sectionOneHeadlineMiddleContainer')
+    var mainContainer = document.querySelector('.indiaFightCovidThird')
+
+    data.forEach(function(ele){
+        var newDiv = document.createElement('div')
+        // newDiv.classList.add('sectionOneHeadlineMiddleCard')
+        var newA = document.createElement('img')
+        newA.src = ele.imageURL
+        newDiv.append(newA)
+        newP = document.createElement('p')
+        newP.innerText = ele.title
+        newDiv.append(newP)
+        container.append(newDiv)
+    })
+
+    mainContainer.append(container)
+})();
+
+(async function lifestyle(){
+    let data = await fetch('http://localhost:3000/lifestyle')
+    data = await data.json()
+    console.log(data);
+    let mainContainer = document.querySelector('.lifestyle')
+    let container = document.createElement('div')
+    data.forEach(function(ele){
+        let mainDiv = document.createElement('div')
+        let newP = document.createElement('p')
+        newP.innerText = ele.mainTitle
+        let newImg = document.createElement('img')
+        newImg.src = ele.imageURL
+        let innerDiv = document.createElement('div')
+        let innerPOne = document.createElement('p')
+        innerPOne.innerText = ele.titleOne
+        let innerPTwo = document.createElement('p')
+        innerPTwo.innerText = ele.titleTwo
+        innerDiv.append(innerPOne)
+        innerDiv.append(innerPTwo)
+
+        mainDiv.append(newP)
+        mainDiv.append(newImg)
+        mainDiv.append(innerDiv)
+        container.append(mainDiv)
     })
     mainContainer.append(container)
 })();
