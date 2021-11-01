a3599c0e12dc039f66f4a6b20eb217ce61e0fe9e
Mausam Kumar <mausam15171098@gmail.com>
2021-11-01 21:36:28 +0530

    completed till event section

--- jsFile/city.js
+++ jsFile/city.js
@@ -13,3 +13,37 @@
     mainContainer.append(container)
 })();
 
+(async function metroNews(){
+    let data = await fetch('http://localhost:3000/metroCity')
+    data = await data.json()
+    var container = document.createElement('div')
+    var mainContainer = document.querySelector('.metroNews')
+    console.log(data);
+    data.forEach(function(ele){
+        let mainDiv = document.createElement('div')
+
+        mainDiv.setAttribute('class', 'metroNewsCard')
+        let divOne = document.createElement('div')
+        let divTwo = document.createElement('div')
+        let divThree = document.createElement('div')
+
+        let imageTitle = document.createElement('p')
+        let titleOne = document.createElement('p')
+        let titleTwo = document.createElement('p')
+        let titleThree = document.createElement('p')
+
+        divTwo.style.backgroundImage = `url(${ele.imageURL})`;
+        imageTitle.innerText = ele.imageTitle
+        titleOne.innerText = ele.titleOne
+        titleTwo.innerText = ele.titleTwo
+        titleThree.innerText = ele.titleThree
+        divOne.innerText = ele.mainTitle
+        divTwo.append(imageTitle)
+        divThree.append(titleOne,titleTwo,titleThree)
+        mainDiv.append(divOne,divTwo,divThree)
+        container.append(mainDiv)
+
+    })
+
+    mainContainer.append(container)
+})();
