558a8ed2c65c1ca933dbd54857cb143951dc3502
Mausam Kumar <mausam15171098@gmail.com>
2021-11-05 23:34:27 +0530

    completed entertainment page

--- jsFile/entertainment.js
+++ jsFile/entertainment.js
@@ -1,3 +1,17 @@
+// Header Section
+(async function addHeader(){
+    let data = await fetch('http://localhost:3000/headerTag')
+    data = await data.json()
+    var container = document.querySelector('.headerBottomContainer')
+    var mainContainer = document.querySelector('.headerBottom')
+    data.forEach(function(ele){
+        var newA = document.createElement('a')
+        newA.innerText = ele.name
+        newA.href = ele.href
+        container.append(newA)
+    })
+    mainContainer.append(container)
+})();
 
 window.addEventListener("load", () => {
     const m_parent = document.getElementById("music");
